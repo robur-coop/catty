abfedd4ffc8c6b8830ea49e83695f210d6536ce2
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-03-20 11:57:39 +0100

    Use basic ring buffer instead of ke

--- src/rb.ml
+++ src/rb.ml
@@ -10,36 +10,31 @@ type wo = < wr: unit >
 type 'a rd = < rd: unit ; .. > as 'a
 type 'a wr = < wr: unit ; .. > as 'a
 
-let to_power_of_two v =
-  let res = ref (pred v) in
-  res := !res lor (!res lsr 1)
-  ; res := !res lor (!res lsr 2)
-  ; res := !res lor (!res lsr 4)
-  ; res := !res lor (!res lsr 8)
-  ; res := !res lor (!res lsr 16)
-  ; succ !res
-
-let make len =
-  {arr= Array.make (to_power_of_two len) None; rd_cursor= 0; wr_cursor= 0}
+let make len = {arr= Array.make len None; rd_cursor= 0; wr_cursor= 0}
 
 exception Full
 exception Empty
 
-let length t = t.wr_cursor - t.rd_cursor
-let is_empty t = t.rd_cursor = t.wr_cursor
-let available t = Array.length t.arr - (t.wr_cursor - t.rd_cursor)
+let length t =
+  if t.rd_cursor <= t.wr_cursor then t.wr_cursor - t.rd_cursor
+  else
+    let len = Array.length t.arr in
+    len - t.rd_cursor + t.wr_cursor
+
+let is_empty t = length t = 0
+let available t = Array.length t.arr - length t
 let is_full t = length t = Array.length t.arr
-let mask t v = v land (Array.length t.arr - 1)
+let mask t v = v mod Array.length t.arr
 
 let push t v =
   if is_full t then raise Full
-  ; t.arr.(mask t t.wr_cursor) <- Some v
-  ; t.wr_cursor <- t.wr_cursor + 1
+  ; t.arr.(t.wr_cursor) <- Some v
+  ; t.wr_cursor <- mask t (t.wr_cursor + 1)
 
 let pop t =
   if is_empty t then raise Empty
-  ; let[@warning "-8"] (Some v) = t.arr.(mask t t.rd_cursor) in
-    t.rd_cursor <- t.rd_cursor + 1
+  ; let[@warning "-8"] (Some v) = t.arr.(t.rd_cursor) in
+    t.rd_cursor <- mask t (t.rd_cursor + 1)
     ; v
 
 let fit_and_push t v =
